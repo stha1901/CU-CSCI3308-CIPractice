e45f572057392e76c4ec325daaa728c17f02085f
Stacy <stha1901@colorado.edu>
2018-04-19 14:52:13 -0600

    Test travis

--- geometry.c
+++ geometry.c
@@ -69,3 +69,11 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->y = ((a->y + b->y) / 2.0 );
 
 }
+
+double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c){
+    part1 = a->x*(b->y-c->y);
+    part2 = b->x*(c->y-a->y);
+    part3 = c->x*(a->y-b->y);
+    area = (part1+part2+part3)/2;
+    return area;
+}
